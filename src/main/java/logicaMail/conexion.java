e840beff17f807fd42e6c6fe99711feca318be19
Nando1702 <120483561+Nando1702@users.noreply.github.com>
2024-03-14 18:36:18 +0100

    Conexion

--- src/main/java/logicaMail/conexion.java
+++ src/main/java/logicaMail/conexion.java
@@ -4,10 +4,48 @@
  */
 package logicaMail;
 
+import gui.LoginFrame;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.AuthenticationFailedException;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author usuario
  */
-public class conexion {
-    
+public class Conexion {
+
+    private Transport transport;
+
+    public int loginTransport(String email, String password) {
+
+        Properties smtpProps = new Properties();
+        smtpProps.setProperty("mail.smtp.host", "smtp.gmail.com");
+        smtpProps.setProperty("mail.smtp.auth", "true");
+        smtpProps.setProperty("mail.smtp.starttls.enable", "true");
+        smtpProps.setProperty("mail.smtp.port", "587");
+
+        Session session = Session.getDefaultInstance(smtpProps);
+
+        try {
+            transport = session.getTransport("smtp");
+            transport.connect(email, new String(password));
+            return 1;
+        } catch (AuthenticationFailedException ex) {
+            return 2;
+        } catch (NoSuchProviderException ex) {
+            Logger.getLogger(LoginFrame.class.getName()).log(Level.SEVERE, null, ex);
+            return 3;
+        } catch (MessagingException ex) {
+            Logger.getLogger(LoginFrame.class.getName()).log(Level.SEVERE, null, ex);
+            return 4;
+        }
+
+    }
 }
